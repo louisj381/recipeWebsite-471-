c8d862228bd87f076d300ddc0f5c3130c06f20a4
raemcphail <raemcphail@gmail.com>
2018-12-05 11:54:24 -0700

    Create standard user

--- CreateStdAccount.php
+++ CreateStdAccount.php
@@ -1,16 +1,35 @@
 <?php
 ob_start();
 session_start();
+//TODO make unique
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  $username = mysqli_real_escape_string($db,$_POST['username']);
+  $password = mysqli_real_escape_string($db,$_POST['password']);
+  $email = mysqli_real_escape_string($db,$_POST['email']);
+
+  echo $username;
+  echo $password;
+  echo $email;
+
+  // $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+  // $result = mysqli_query($db,$sql);
+  // $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  // $active = $row['active'];
+}
  ?>
 
-<html>
-<body>
-  <form action="homepage.php" method="post">
+ <html>
+ <body>
+   <form action="homepage.php" method="post">
 
-  Username: <input type="text" name="Username" value="<?php echo $name;?>"><br><br>
-  Password: <input type="password" name="Password" value="<?php echo $name;?>"><br>
-  Email: <input type="text" name="Username" value="<?php echo $name;?>"><br>
-  <input type="submit" name="createStdAccount" value="submit">
-</form>
-</body>
-</html?>
+   Username: <input type="text" name="username" value="<?php echo $username;?>"><br><br>
+   Password: <input type="password" name="password" value="<?php echo $password;?>"><br>
+    Email: <input type="text" name="email" value="<?php echo $email;?>"><br>
+   <input type="submit" name="btnsubmit" value="submit">
+ </form>
+   <form action="index.php" method="post">
+     <input type="submit" name="createStdAccount" value="Create Standard User">
+   </form>
+ </body>
+ </html?>
