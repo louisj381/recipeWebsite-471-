8e4b16c79bb00a0a7ebf78622d70e06a660311d9
Louis Johnson <louisj381@gmail.com>
2018-12-05 03:35:09 -0700

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<form action="browse.php" method="post">
+  <input type="submit" name="browse" value="Browse">
+</form>
+<form action="cookbook.php" method="post">
+  <input type="submit" name="cookbook" value="Cookbook">
+</form>
+<form action="pantry.php" method="post">
+  <input type="submit" name="pantry" value="Pantry">
+</form>
+<form action="guestbook.php" method="post">
+  <input type="submit" name="guestbook" value="Guestbook">
+</form>
+<form action="account.php" method="post">
+  <input type="submit" name="account" value="Account">
+</form>
+<form action="login.php" method="post">
+  <input type="submit" name="logout" value="Logout">
+</form>
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,3 @@
+<?php
+echo "I moved here";
+?>
