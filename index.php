43d37cada88c390cd7758b917a2c2124df06aeb4
Louis Johnson <louisj381@gmail.com>
2018-12-05 09:52:19 -0700

    added starter functions

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+ob_start();
+session_start();
+ ?>
+
+<html>
+<body>
+  <form action="homepage.php" method="post">
+
+  Username: <input type="text" name="Username" value="<?php echo $name;?>"><br><br>
+  Password: <input type="password" name="Password" value="<?php echo $name;?>"><br>
+  <input type="submit" name="login" value="login">
+</form>
+  <form action="CreateStdAccount.php" method="post">
+    <input type="submit" name="createStdAccount" value="Create Standard User">
+  </form>
+  <form action="CreateCurAccount.php" method="post">
+    <input type="submit" name="createCurAccount" value="Create Curator">
+  </form>
+</body>
+</html?
