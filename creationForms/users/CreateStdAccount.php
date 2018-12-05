951c179604a52c47e4bb0e3ce73f2515a9bbcbfa
raemcphail <raemcphail@gmail.com>
2018-12-05 12:03:25 -0700

    fixed merge, added to std user account php file

--- creationForms/users/CreateStdAccount.php
+++ creationForms/users/CreateStdAccount.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
   ob_start();
   session_start();
   define(root, "../../");
@@ -23,4 +23,39 @@
       <input type="submit" name="createStdAccount" value="submit">
     </form>
   </body>
-</html?>
+</html?> -->
+<?php
+ob_start();
+session_start();
+//TODO make unique
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  $username = mysqli_real_escape_string($db,$_POST['username']);
+  $password = mysqli_real_escape_string($db,$_POST['password']);
+  $email = mysqli_real_escape_string($db,$_POST['email']);
+
+  echo $username;
+  echo $password;
+  echo $email;
+
+  // $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+  // $result = mysqli_query($db,$sql);
+  // $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+  // $active = $row['active'];
+}
+ ?>
+
+ <html>
+ <body>
+   <form action="homepage.php" method="post">
+
+   Username: <input type="text" name="username" value="<?php echo $username;?>"><br><br>
+   Password: <input type="password" name="password" value="<?php echo $password;?>"><br>
+    Email: <input type="text" name="email" value="<?php echo $email;?>"><br>
+   <input type="submit" name="btnsubmit" value="submit">
+ </form>
+   <form action="index.php" method="post">
+     <input type="submit" name="createStdAccount" value="Create Standard User">
+   </form>
+ </body>
+ </html?>
