0bad5c9d2a2a71dc9fbc3502d88da8c4c853c145
raemcphail <raemcphail@gmail.com>
2018-12-06 02:10:48 -0700

    meal.php was added, it's an exact copy of recipe.php, no changes yet

---
+++ tables/meals.php
@@ -0,0 +1,50 @@
+<?php
+  ob_start();
+  session_start();
+ ?>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="../styles/body_styles.css">
+    <link rel="stylesheet" href="../styles/table_styles.css">
+  </head>
+  <body>
+    <table>
+      <tr>
+        <th>Meal</th>
+        <th>Type</th>
+        <th>Recipes</th>
+        <th>Instructions</th>
+      </tr>
+      <!-- next rows -->
+      <?php
+        $root = "../";
+        include($root . "connection/dbConfig.php");  //to access db
+        $uID = $_SESSION['user_id'];
+        $sqlText = $_SESSION['sqlBrowseMeal'];
+        //echo $_PATH['search'] . "<-";
+        $res = $db->query($sqlText);
+        if (empty($_SESSION['sqlBrowseMeal'])) {
+          //do nothing
+        }elseif ( $res->num_rows > 0 ) {
+          while ( $row = $res->fetch_assoc() ) {
+            $name = $row['Name'];
+            $prep = $row['PrepTime'];
+            #cooktime
+            $rating = $row['Rating'];
+            $instructions = $row['Instructions'];
+            echo "
+            <tr>
+             <td>$name</td>
+             <td>$prep</td>
+             <td>$rating</td>
+             <td>$instructions</td>
+            </tr>";
+          }
+        } else {
+          echo "<tr><td>None</td><td></td></tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
