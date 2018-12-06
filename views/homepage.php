32381db535751076252c98a50c65cbf9a627624d
Keenan Gaudio <keenangaudio@me.com>
2018-12-06 00:26:36 -0700

    made things pretty

--- views/homepage.php
+++ views/homepage.php
@@ -12,13 +12,16 @@
   <link rel="stylesheet" href="../styles/body_styles.css">
 </head>
   <body>
-    <div style="align: center;">
-      <button class="button" style="width:50%;" onClick="location.href = '../views/browse.php'">Browse</button>
-      <button class="button" style="width:50%;" onClick="location.href = '../myStuff/cookbook.php'">Cookbook</button>
-      <button class="button" style="width:50%;" onClick="location.href = '../myStuff/pantry.php'">Pantry</button>
-      <button class="button" style="width:50%;" onClick="location.href = '../myStuff/guestbook.php'">Guestbook</button>
-      <button class="button" style="width:50%;" onClick="location.href = '../myStuff/account.php'">Account</button>
-      <button class="button" style="width:50%;" onClick="location.href = '../index.php'">Logout</button>
+    <div class="column4"><p>&nbsp;</p></div>
+    <div class="column">
+      <p>&nbsp;</p>
+      <button class="button" style="width:100%;" onClick="location.href = '../views/browse.php'">Browse</button>
+      <button class="button" style="width:100%;" onClick="location.href = '../myStuff/cookbook.php'">Cookbook</button>
+      <button class="button" style="width:100%;" onClick="location.href = '../myStuff/pantry.php'">Pantry</button>
+      <button class="button" style="width:100%;" onClick="location.href = '../myStuff/guestbook.php'">Guestbook</button>
+      <button class="button" style="width:100%;" onClick="location.href = '../myStuff/account.php'">Account</button>
+      <button class="button" style="width:100%;" onClick="location.href = '../index.php'">Logout</button>
     </div>
+    <div class="column4"><p>&nbsp;</p></div>
   </body>
 </html>
