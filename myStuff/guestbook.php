8766b8fef4b31a3af77e1aaf433ce0e6ba96d725
Louis Johnson <louisj381@gmail.com>
2018-12-05 17:41:37 -0700

    Things are a changin

--- myStuff/guestbook.php
+++ myStuff/guestbook.php
@@ -1,40 +1,30 @@
 <?php
   ob_start();
   session_start();
-  include("../connection/dbConfig.php");
-  $userID = $_SESSION['user_id'];
-  echo $userID;
-
-  $sql = "SELECT * FROM DEPENDANTS WHERE User_Id = 'userID'";
-  $result = mysqli_query($db,$sql);
-
-  while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-      print_r($row);
-      // do stuff with $row
-  }
-
  ?>
-
-
 <html>
-<head>
-  <title>Guestbook</title>
-</head>
-<body>
-    <header>
-      <text><b>Your Dependants</b></text>
-    </header>
-    <textarea name="comment" rows="10" cols="60"></textarea>
-    <header>
-      <text><b>Their Allergies</b></text>
-    </header>
-    <textarea name="comment" rows="10" cols="60"></textarea>
-    <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post">
-      <button class="btn" style="float:right" onclick="">Add</button>
-      <button class="btn" style="float:right" >Edit</button>
-      <button class="btn" style="float:right" >Remove</button>
-   </form>
+  <head>
+    <title> Cake. </title>
+    <!--
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      -->
+    <link rel="stylesheet" href="../styles/body_styles.css">
+  </head>
+  <h2> Guestbook: </h2>
+<div class="column">
+  <iframe src="../tables/dependants.php"></iframe>
+</div>
+<div class="column">
+ <body>
 
 
-</body>
+     <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post">
+       <input type="submit" name="Refresh" value="Refresh">
+     </form>
+     <form action="../views/homepage.php" method="post">
+       <input type="submit" name="back" value="Back to My Stuff">
+     </form>
+
+ </body>
+</div>
 </html>
---
+++ tables/dependants.php
@@ -0,0 +1,43 @@
+<?php
+  ob_start();
+  session_start();
+ ?>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="../styles/table_styles.css">
+  </head>
+  <body>
+    <table id=fancyTable>
+      <tr>
+        <th>Ingredient</th>
+        <th>Number</th>
+      </tr>
+      //next rows
+      <?php
+        $root = "../";
+        include($root . "connection/dbConfig.php");  //to access db
+        $uID = $_SESSION['user_id'];
+
+        $sqlText = "SELECT * FROM DEPENDANTS WHERE User_Id =" . $uID .";";
+        //echo $sqlText;
+        $res = $db->query($sqlText);
+        if ( $res->num_rows > 0 ) {
+          while ( $row = $res->fetch_assoc() ) {
+            $name = $row['Name'];
+            $relationship = $row['Relationship'];
+            $allergies = $row['No-of_allergies']; //check this
+            echo "
+            <tr>
+             <td>$name</td>
+             <td>$relationship</td>
+             <td>$allergies</td>
+            </tr>";
+          }
+        } else {
+          echo "<tr><td>None</td><td></td></tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
